f6908e586f517f8eea84d8bfe813e2fb7b7ce5b2
Nukita <nikita.kostenko23@mail.ru>
2023-12-23 21:54:03 +0300

    added a script for the mobile menu

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var mobileMenuIconEl = document.getElementById("mobile-menu-icon");
+    var mobileMenuEl = document.getElementById("mobile-menu-outter");
+
+    mobileMenuIconEl.addEventListener("click", () => {
+        mobileMenuEl.style.display = "block";
+    });
+
+    document.addEventListener("click", (event) => {
+        if (event.target != mobileMenuIconEl) {
+            mobileMenuEl.style.display = "none";
+        }
+    });
+});
 const activeCard = document.getElementsByClassName(".active");
 console.log(activeCard)
