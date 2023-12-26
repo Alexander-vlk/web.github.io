41165a6d4a23cc05caca5b185a196ce6ebabd626
Alexander Barmenkov <www.lad282@gmail.com>
2023-12-26 18:54:13 +0300

    webform and slider number 2 partially made

--- index.js
+++ index.js
@@ -8,31 +8,88 @@ document.addEventListener('DOMContentLoaded', function () {
         interval: 5000,
         type  : 'loop',
     });
-
-    var splide_partners_1 = new Splide('#splide1', {
-        perPage: 1,
-        width: '100%',
-        perMove: 1,
-        gap: '2rem',
-        autoplay: false,
-        interval: 5000,
-        type  : 'loop',
-    });
-
-    var splide_partners_2 = new Splide('#splide2', {
-        perPage: 1,
-        width: '100%',
-        perMove: 1,
-        gap: '2rem',
-        autoplay: false,
-        interval: 5000,
-        type  : 'loop',
-    });
     
     splide_reviews.mount();
-    // splide_partners_1.mount();
-    // splide_partners_2.mount();
+
+    var clientsSlider1 = new Splide( '#clients-gallery-1', {
+        type: "loop",
+        width: "140%",
+        perMove: 1,
+        perPage: 14,
+        gap: "1em",
+        autoWidth: true,
+        arrows: false,
+        pagination: false,
+        drag: false,
+        autoplay: true,
+        interval: 4000,
+        speed: 1200,
+        breakpoints: {
+            780: {
+                height: "100px",
+            }
+        }
+    });
+    clientsSlider1.mount();
+
+    var clientsSlider2 = new Splide( '#clients-gallery-2', {
+        type: "loop",
+        width: "140%",
+        perMove: 1,
+        perPage: 16,
+        gap: "1em",
+        autoWidth: true,
+        arrows: false,
+        pagination: false,
+        drag: false,
+        autoplay: true,
+        interval: 5000,
+        speed: 1200,
+    });
+    clientsSlider2.mount();
 });
+
+$(function(){
+    $(".formcarryForm").submit(function(e){
+      e.preventDefault();
+      var href = $(this).attr("action");
+      
+      $.ajax({
+          type: "POST",
+          url: href,
+          data: new FormData(this),
+          dataType: "json",
+          processData: false,
+          contentType: false,
+          success: function(response){
+            if(response.status == "success"){
+                alert("We received your submission, thank you!");
+            }
+            else if(response.code === 422){
+              alert("Field validation failed");
+              $.each(response.errors, function(key) {
+                $('[name="' + key + '"]').addClass('formcarry-field-error');
+              });
+            }
+            else{
+              alert("An error occured: " + response.message);
+            }
+          },
+          error: function(jqXHR, textStatus){
+            const errorObject = jqXHR.responseJSON
+  
+            alert("Request failed, " + errorObject.title + ": " + errorObject.message);
+          },
+          complete: function(){
+            history.pushState({ formOpen: false }, '', '/');
+            popupForm.style.display = 'none';
+          }
+      });
+    });
+  });
+
+
+
 
 const startCard = document.getElementById("start-card");
 const mainCard = document.getElementById("main-card");
@@ -53,3 +110,4 @@ document.addEventListener("DOMContentLoaded", () => {
         mainCard.style.transform = "scale(1)";
     });
 });
+
